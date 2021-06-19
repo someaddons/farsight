67cc92e2e52e630a72870e8e69d95a7a567078de
someaddons <someaddons@github.com>
2021-06-19 16:51:02 +0200

    v1.3 Remove empty event handler callback

--- src/main/java/com/farsight/event/EventHandler.java
+++ src/main/java/com/farsight/event/EventHandler.java
@@ -1,16 +1,8 @@
 package com.farsight.event;
 
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-
 /**
  * Forge event bus handler, ingame events are fired here
  */
 public class EventHandler
 {
-    @SubscribeEvent
-    public static void onWorldTick(final TickEvent.WorldTickEvent event)
-    {
-
-    }
 }
