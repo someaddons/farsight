9cc8639bce569ca76fc36ef348eab128ba9ee6cf
someaddons <someaddons@github.com>
2023-08-22 18:37:00 +0200

    1.6 fixes

--- src/main/java/com/farsight/FarsightMod.java
+++ src/main/java/com/farsight/FarsightMod.java
@@ -6,17 +6,16 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Random;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(modid = FarsightMod.MODID)
+@Mod(modid = FarsightMod.MODID, acceptableRemoteVersions = "*", acceptedMinecraftVersions = "*", version = "1.6")
 public class FarsightMod
 {
-    public static final String MODID  = "farsight_view";
+    public static final String MODID  = "farsight";
     public static final Logger LOGGER = LogManager.getLogger();
 
     public static Random rand = new Random();
 
     public FarsightMod()
     {
-
+        LOGGER.info("Farsight initialized");
     }
 }
