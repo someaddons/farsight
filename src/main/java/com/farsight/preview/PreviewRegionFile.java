fa73c24cd6e338085d229585392921221ce1f116
someaddons <someaddons@github.com>
2026-06-04 19:38:04 +0200

    5.1

--- src/main/java/com/farsight/preview/PreviewRegionFile.java
+++ src/main/java/com/farsight/preview/PreviewRegionFile.java
@@ -295,7 +295,6 @@ public class PreviewRegionFile implements Closeable
 
         if (serverData == null)
         {
-            FarsightMod.LOGGER.warn("No server found, cannot create path");
             return null;
         }
 
