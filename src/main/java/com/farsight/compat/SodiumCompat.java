855e67f2f33ef2053c3375ec0384fc08d5f09746
someaddons <someaddons@github.com>
2026-06-19 17:03:50 +0200

    5.2: Improved mod compat, chunk preview is now enabled by default(new configs)

--- src/main/java/com/farsight/compat/SodiumCompat.java
+++ src/main/java/com/farsight/compat/SodiumCompat.java
@@ -1,16 +1,9 @@
 package com.farsight.compat;
 
-import com.farsight.FarsightClientChunkManager;
-import net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkStatus;
-import net.caffeinemc.mods.sodium.client.render.chunk.map.ChunkTrackerHolder;
-
 public class SodiumCompat
 {
     public static void init()
     {
-        FarsightClientChunkManager.loadCallback.add(((clientLevel, levelChunk) -> ChunkTrackerHolder.get(clientLevel)
-          .onChunkStatusAdded(levelChunk.getPos().x(), levelChunk.getPos().z(), ChunkStatus.FLAG_ALL)));
-        FarsightClientChunkManager.unloadCallback.add(((clientLevel, levelChunk) -> ChunkTrackerHolder.get(clientLevel)
-          .onChunkStatusRemoved(levelChunk.getPos().x(), levelChunk.getPos().z(), ChunkStatus.FLAG_ALL)));
+
     }
 }
