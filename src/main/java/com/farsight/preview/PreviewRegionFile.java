b84ae7bc5156a224e6d31454155851cd3816cedb
someaddons <someaddons@github.com>
2026-09-10 14:56:44 +0200

    Catch throwables instead of exception

--- src/main/java/com/farsight/preview/PreviewRegionFile.java
+++ src/main/java/com/farsight/preview/PreviewRegionFile.java
@@ -102,7 +102,7 @@ public class PreviewRegionFile implements Closeable
                 checkCompact();
             }
         }
-        catch (Exception e)
+        catch (Throwable e)
         {
             FarsightMod.logDebug("Failed to initialize region file:" + path, e);
             clearFile();
@@ -234,7 +234,7 @@ public class PreviewRegionFile implements Closeable
             channel.position(0);
             initializeFile();
         }
-        catch (Exception e)
+        catch (Throwable e)
         {
             FarsightMod.logDebug("Failed to reset file:" + path, e);
         }
@@ -399,7 +399,7 @@ public class PreviewRegionFile implements Closeable
             ClientboundLevelChunkWithLightPacket packet = new ClientboundLevelChunkWithLightPacket(registryFriendlyByteBuf);
             return packet;
         }
-        catch (Exception e)
+        catch (Throwable e)
         {
             // On Error: reset file as its format is not compatible or some other I/O error occured
             FarsightMod.logDebug("Failed to decode chunk for file: " + path + " resetting file", e);
